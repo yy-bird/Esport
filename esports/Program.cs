9b62c2504fbfe32bfe1f4211fb32e91891b3eff2
starbadboy <st>
2019-02-02 14:36:41 +0800

    add

--- esports/Program.cs
+++ esports/Program.cs
@@ -19,6 +19,6 @@ namespace esports
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>(); 
     }
 }
